3aa7cd76d65c81b68e64d9e0b95048d7eaaecf07
lasvistass <spinelli1k90@gmail.com>
2023-02-27 21:53:23 +0100

    conf parte 1 -v 0.0.1

--- generators/app/index.js
+++ generators/app/index.js
@@ -16,17 +16,36 @@ module.exports = class extends Generator {
       const className = path.basename(inputFile, path.extname(inputFile));
       const inputFileData = fs.readFileSync(inputFile, 'utf8');
       const inputData = JSON.parse(inputFileData);
-      const outputFile = `src/app/module/${className}.ts`;
+      const outputFile1 = `src/app/module/${className}.ts`;
+      const outputFile2 = `src/app/component/${className}.ts`;
       const outputFields = Object.keys(inputData).map((key) => {
         return `  ${key}: ${typeof inputData[key]};`
       }).join('\n');
+      const outputFields2 = Object.keys(inputData).filter((key) => key != "id").map((key) => {
+        return `  ${key}: {
+          title: '${key}'
+        },`
+      }).join('\n');
 
       this.fs.write(
-        this.destinationPath(outputFile),
+        this.destinationPath(outputFile1),
         `
 import { UserGroup } from './usergroup';
 
 export class ${className} extends test {\n${outputFields}\n}`
+      );
+
+      this.fs.write(
+        this.destinationPath(outputFile2),
+        `
+import merge from 'lodash/merge';
+import {tableCommonSettings} from "../entity-table/entity-table.conf";
+
+const tableSettings = {
+  columns: {
+    \n${outputFields2}\n
+  },
+};`
       );
     });
   }
