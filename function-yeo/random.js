a6bc9f9bc4c6b22d0e95b19d70be17ffbd20a0a9
repospin <spinellinot@gmail.com>
2023-03-16 13:18:48 +0100

    fix random

--- function-yeo/random.js
+++ function-yeo/random.js
@@ -1,11 +1,11 @@
 function generateRandomAlphaNumeric() {
     var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var characters = '123456789';
     var charactersLength = characters.length;
-    for ( var i = 0; i < 20; i++ ) {
+    for ( var i = 0; i < 19; i++ ) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    return result;
+    return result + 'L';
   }
   
   module.exports = {
