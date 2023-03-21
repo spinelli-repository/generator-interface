9e30b9262d6117e766b3b602abe8ccfbfe8aab9f
repospin <spinellinot@gmail.com>
2023-03-21 11:36:43 +0100

    long too big Alexandra fix

--- function-yeo/random.js
+++ function-yeo/random.js
@@ -1,13 +1,7 @@
 function generateRandomLong() {
-    var result = '';
-    var characters = '1234567890';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < 16; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return '000' + result + 'L';
+    return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER;
   }
-  
+
   module.exports = {
     generateRandomLong: generateRandomLong
   };
