df3d4f72ccfc12b8387ce32ae20aac25e8258be0
repospin <spinellinot@gmail.com>
2023-03-21 10:50:32 +0100

    long too big

--- function-yeo/random.js
+++ function-yeo/random.js
@@ -2,10 +2,10 @@ function generateRandomLong() {
     var result = '';
     var characters = '1234567890';
     var charactersLength = characters.length;
-    for ( var i = 0; i < 19; i++ ) {
+    for ( var i = 0; i < 16; i++ ) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    return result + 'L';
+    return '000' + result + 'L';
   }
   
   module.exports = {
