acbdf9afb768088c61e2d07233c401d311179822
Simone Spinelli <105516267+spinelli-git@users.noreply.github.com>
2023-09-28 11:24:58 +0200

    Add edit cells

--- generators/app/index.js
+++ generators/app/index.js
@@ -93,6 +93,10 @@ module.exports = class extends Generator {
           type: 'custom',
           component: DateRangePickerFilterComponent,
       },
+      editor: {
+            type: 'custom',
+            component: DatePickerEditorComponent,
+      },
     },`;
         }
         if(key == 'uid'){
